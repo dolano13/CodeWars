51849c553f93f434572b818f229d4435a292b765
Daniela Olano <danielaolano96@gmail.com>
2019-06-10 22:04:51 -0400

    jun 10

--- Katas.js
+++ Katas.js
@@ -1,2 +1,15 @@
 //6.10.19
 //https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/javascript
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle");
+}
+//https://www.codewars.com/kata/sum-of-all-the-multiples-of-3-or-5/train/javascript
+function findSum(n) {
+  let answer = 0;
+  for (let i = 3; i <= n; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      answer += i;
+    }
+  }
+  return answer;
+}
