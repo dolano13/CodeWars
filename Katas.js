6e4f8bc0ac084048f16f3c57e87b1ec47bf35162
Daniela Olano <danielaolano96@gmail.com>
2019-06-12 21:27:21 -0400

    6.12

--- Katas.js
+++ Katas.js
@@ -19,3 +19,44 @@ function findSum(n) {
 function maps(x) {
   return x.map(num => num * 2);
 }
+
+//6.12.19
+//https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/javascript
+function sumTwoSmallestNumbers(numbers) {
+  var small = numbers[0];
+  let [a, b] = numbers.sort((a, b) => a - b);
+  return a + b;
+}
+//https://www.codewars.com/kata/abbreviate-a-two-word-name/train/javascript
+function abbrevName(name) {
+  let x = name.split(" ");
+  return (x[0][0] + "." + x[1][0]).toUpperCase();
+}
+//https://www.codewars.com/kata/welcome/train/javascript
+function greet(language) {
+  let greeting = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso"
+  };
+  if (greeting[language]) {
+    return greeting[language];
+  } else {
+    return greeting.english;
+  }
+}
+//https://www.codewars.com/kata/simple-multiplication/train/javascript
