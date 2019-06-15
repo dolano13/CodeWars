066a339b52a3c59b4708121fceeb44ea1f36f8c3
Daniela Olano <danielaolano96@gmail.com>
2019-06-15 12:34:06 -0400

    jun14

--- Katas.js
+++ Katas.js
@@ -117,4 +117,31 @@ function betterThanAverage(classPoints, yourPoints) {
 function makeUpperCase(str) {
   return str.toUpperCase();
 }
-//
+//https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+function otherAngle(a, b) {
+  return 180 - (a + b);
+}
+//6.15.19
+//https://www.codewars.com/kata/total-amount-of-points/train/javascript
+function points(games) {
+  let points = 0;
+  for (let i = 0; i < games.length; i++) {
+    let each = games[i].split(":");
+    if (each[0] > each[1]) {
+      points += 3;
+    } else if (each[0] == each[1]) {
+      points += 1;
+    } else {
+      points += 0;
+    }
+  }
+  return points;
+}
+//https://www.codewars.com/kata/grasshopper-summation/train/javascript
+var summation = function(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+};
