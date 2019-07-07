f9c49e242fb3e1113b82d8e73c69595d00e714ec
Daniela Olano <danielaolano96@gmail.com>
2019-07-07 16:54:28 -0400

    7.7 kata

--- Katas.js
+++ Katas.js
@@ -401,3 +401,12 @@ function sumTriangularNumbers(n) {
     return total;
   }
 }
+//7.7.18
+//https://www.codewars.com/kata/56747fd5cb988479af000028/train/javascript
+function getMiddle(s) {
+  if (s.length % 2 === 0) {
+    return s[s.length / 2 - 1] + s[s.length / 2];
+  } else {
+    return s[Math.floor(s.length / 2)];
+  }
+}
